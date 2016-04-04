e51a2190e6192387a892bb07bafb3eb4983e13ee
ricebullet <nicholasreis@gmail.com>
2016-04-03 19:30:35 -0700

    cleaned up layout

--- gre_game.py
+++ gre_game.py
@@ -13,14 +13,17 @@ class Data(object):
     """Contains all the data functions to display the answer, definition, and
     choices"""
 
-    def collect_words(word_file="gre_word_file.txt"):
+    def __init__(self, words=None):
+        if not words:
+            self.words = self.collect_words()
+        else:
+            self.words = self.words
+        self.lookup = PyDictionary()
+
+    def collect_words(self, word_file="gre_word_file.txt"):
         """Builds the word database"""
         wordlist = open(word_file).read().splitlines()
         return wordlist
-
-    def __init__(self, words=collect_words()):
-        self.words = words
-        self.lookup = PyDictionary()
 
     def display_words(self):
         for word in self.words:
@@ -29,7 +32,7 @@ class Data(object):
     def answer(self):
         """Chooses a random word from the wordlist"""
         answer_idx = random.randint(0, len(self.words) - 1)
-        answer = self.words[answer_idx]
+        answer = self.words[answer_idx].lower()
         del self.words[answer_idx]
         return answer
     def definition(self, answer):
@@ -55,7 +58,7 @@ class Data(object):
         print 'Please type the answer 3x, each on its own line.\n'
         count = 0
         while count < 3:
-            word = raw_input('> ')
+            word = raw_input('> ').lower()
             if word == answer:
                 count += 1
             else:
@@ -83,17 +86,15 @@ class Game(object):
     def start(self):
         """Prompt to choose easy = number version or hard = word version."""
         self.clear_screen()
-        game_on = True
-        while game_on:
-            choice = raw_input('\nWould you like the easy or hard version? \n').lower()
-            if choice.startswith('h'):
-                self.reset_score()
-                self.word_game()
-            elif choice.startswith('e'):
+        self.game_on = True
+        while self.game_on:
+            choice = raw_input('Would you like the easy or hard version? \n').lower()
+            if choice == 'easy':
                 self.reset_score()
                 self.num_game()
-                print 'Given the definition, choose the correct number that',
-                'corresponds to the word. You have two tries.'
+            elif choice == 'hard':
+                self.reset_score()
+                self.word_game()
             else:
                 print 'I don\'t know what that means.'
 
@@ -102,15 +103,19 @@ class Game(object):
         while True:
             try:
                 num = int(raw_input('How many questions would you like? (1-20) '))
-            except:
+            except Exception:
                 print "That is not a valid number."
                 continue
-            else:
-                break
+            break
         return num
 
+    def display_engine(self, num):
+        """Prints the definition and choices"""
+
+
     def word_game(self):
-        """Word game layout"""
+        """Word game initializes"""
+        data = Data()
         num = self.num_questions()
         print '\nGiven the definition, type the correct word. You have one try.'
         raw_input('Press Enter to start...')
@@ -134,19 +139,24 @@ class Game(object):
                     print 'Incorrect.'
                     turns += 1
                     if turns == 1:
-                        print 'The answer was %s.' % (answer)
+                        print 'The answer was %s.' % answer
                         data.practice(answer)
             self.attempts += 1
-        print 'Your score is %d/%d.' % (self.wins, self.attempts)
-        print '-' * 10
-        game_on = False
+        self.end()
 
     def num_game(self):
+        print 'Given the definition, choose the correct number that',
+        'corresponds to the word. You have two tries.'
         pass
+
+    def end(self):
+        print '\nGame Over.\nYour score was %d/%d.' % (self.wins, self.attempts)
+        print '-' * 10
+        self.game_on = False
 
     def reset_score(self):
         self.wins, self.attempts = 0, 0
 
 
-data = Data()
 game = Game()
+game.start()
